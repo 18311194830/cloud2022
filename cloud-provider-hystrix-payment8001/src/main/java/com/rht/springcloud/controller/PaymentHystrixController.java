3e739f3aff32d44729ce970ccd635f12382e1aec
18311194830 <hh0415>
2022-05-30 20:12:36 +0800

    hystri实现服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/rht/springcloud/controller/PaymentHystrixController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/rht/springcloud/controller/PaymentHystrixController.java
@@ -1,6 +1,7 @@
 package com.rht.springcloud.controller;
 
 import com.rht.springcloud.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
+@Slf4j
 public class PaymentHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -25,6 +27,14 @@ public class PaymentHystrixController {
     @GetMapping("/payment/hystrix/timeout/{id}")
     public String Hystrix_Timeout(@PathVariable("id") Integer id){
         String result = paymentHystrixService.Hystrix_Timeout(id);
+        return result;
+    }
+
+    //==============服务熔断
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentCircuitBreaker(id);
+        log.info("+++++result:"+result);
         return result;
     }
 }
--- cloud-provider-hystrix-payment8001/src/main/java/com/rht/springcloud/service/PaymentHystrixService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/rht/springcloud/service/PaymentHystrixService.java
@@ -4,4 +4,6 @@ public interface PaymentHystrixService {
     String Hystrix_OK(Integer id);
 
     String Hystrix_Timeout(Integer id);
+
+    public String paymentCircuitBreaker(Integer id);
 }
