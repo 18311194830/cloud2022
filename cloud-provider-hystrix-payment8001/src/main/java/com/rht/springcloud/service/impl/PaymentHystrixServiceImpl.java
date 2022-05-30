8445baf4129386e8f220641ea59808a640545243
18311194830 <hh0415>
2022-05-30 20:12:36 +0800

    hystri实现服务监控

--- cloud-provider-hystrix-payment8001/src/main/java/com/rht/springcloud/service/impl/PaymentHystrixServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/rht/springcloud/service/impl/PaymentHystrixServiceImpl.java
@@ -1,9 +1,12 @@
 package com.rht.springcloud.service.impl;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.rht.springcloud.service.PaymentHystrixService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.concurrent.TimeUnit;
 
@@ -34,6 +37,28 @@ public class PaymentHystrixServiceImpl implements PaymentHystrixService {
         return "线程池" + Thread.currentThread().getName() + "Hystrix_Timeout,id:" + id+"\t"+"o(<||>)o";
     }
 
+    //==============服务熔断
+    @Override
+    @HystrixCommand(fallbackMethod = "testRongDuan_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),  //是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),   //请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),  //时间范围
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60")}//失败率达到多少后跳闸
+    )
+    @RequestMapping("/test/hystrix/rongduan/{id}")
+    public String paymentCircuitBreaker(@PathVariable Integer id){
+        if (id < 0){
+            // 这样做的目的是让他去执行兜底方法
+            throw new RuntimeException("id 不能为负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName()+"\t"+"调用成功，流水号："+serialNumber;
+    }
+
+    public String testRongDuan_fallback(@PathVariable Integer id){
+
+        return "id 不能为负数,降级服务执行…………  id:" + id;
+    }
 
 
 }
